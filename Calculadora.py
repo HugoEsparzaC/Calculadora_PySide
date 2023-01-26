d78a1111bf5920f6abba81d2982319b12f1d26e9
HugoEsparzaC <hugo.esparza3@gmail.com>
2023-01-26 15:02:02 -0600

    Botones

--- Calculadora.py
+++ Calculadora.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow, QLineEdit, QApplication, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QMainWindow, QLineEdit, QPushButton, QGridLayout, QApplication, QWidget, QVBoxLayout
 from PySide6.QtCore import Qt
 class Calculadora(QMainWindow):
     def __init__(self):
@@ -10,6 +10,7 @@ class Calculadora(QMainWindow):
         self.layout_principal = QVBoxLayout()
         self.componente_general.setLayout(self.layout_principal)
         self._crear_area_captura()
+        self._crear_botones()
 
     def _crear_area_captura(self):
         self.linea_entrada = QLineEdit()
@@ -17,6 +18,34 @@ class Calculadora(QMainWindow):
         self.linea_entrada.setAlignment(Qt.AlignRight)
         self.linea_entrada.setReadOnly(True)
         self.layout_principal.addWidget(self.linea_entrada)
+
+    def _crear_botones(self):
+        self.botones = {}
+        layout_botones = QGridLayout()
+        botones = {
+            '7':(0,0),
+            '8':(0,1),
+            '9':(0,2),
+            '/':(0,3),
+            '4':(1,0),
+            '5':(1,1),
+            '6':(1,2),
+            '*':(1,3),
+            '1':(2,0),
+            '2':(2,1),
+            '3':(2,2),
+            '-':(2,3),
+            '0':(3,0),
+            '.':(3,1),
+            'C':(3,2),
+            '+':(3,3),
+            '=':(3,4)
+        }
+        for texto_boton, posicion in botones.items():
+            self.botones[texto_boton] = QPushButton(texto_boton)
+            self.botones[texto_boton].setFixedSize(40,40)
+            layout_botones.addWidget(self.botones[texto_boton], posicion[0], posicion[1])
+        self.layout_principal.addLayout(layout_botones)
 
 if __name__ == '__main__':
     app = QApplication()
